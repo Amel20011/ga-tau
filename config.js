974d85114396003d0c4dd658955630f568b38748
Amel20011 <amelcns8@gmail.com>
2025-12-12 13:28:20 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+{
+  "name": "liviaastore-bot",
+  "version": "1.0.0",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "@whiskeysockets/baileys": "github:kiuur/baileys",
+    "pino": "^8.0.0",
+    "fs-extra": "^11.2.0"
+  }
+}
